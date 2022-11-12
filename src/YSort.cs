ea7c3527d2ae4c593b5c577bc829137007e51407
Jamination <jamesycraft@gmail.com>
2022-11-12 12:36:40 +0000

    woops

--- src/YSort.cs
+++ src/YSort.cs
@@ -3,7 +3,7 @@ namespace BigBoyEngine;
 public class YSort : Node {
     public override void Update() {
         base.Update();
-        foreach (var child in SpatialChildren)
+        foreach (var child in Children)
             child.Depth = (child.Position.Y + 500000) * .000001f;
     }
 }
